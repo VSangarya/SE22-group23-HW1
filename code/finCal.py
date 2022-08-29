f6f11acd7bd0f42332ef002ac2b75486b0807d9f
Ali Qureshi <qureshi.ali@outlook.com>
2022-08-29 15:54:15 -0400

    small change

--- code/finCal.py
+++ code/finCal.py
@@ -18,10 +18,10 @@ def inflation(principal, rate, time):
     if(principal < 0): return "Principal negative, invalid input for function"
 	if(rate < 0): return "Rate negative, invalid input for funtion"
 	if(time < 0): return "Time negative, invalid input for function"
-	return principal*(1+(rate/100)**time)
+	return principal*((1+(rate/100))**time)
 
 def purchasingPower(principal, rate, time):
     if(principal < 0): return "Principal negative, invalid input for function"
 	if(rate < 0): return "Rate negative, invalid input for funtion"
 	if(time < 0): return "Time negative, invalid input for function"
-	return principal/((1+(rate/100)**time))
+	return principal/(((1+(rate/100))**time))
