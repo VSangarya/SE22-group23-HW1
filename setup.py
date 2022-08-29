de5d9be9fef412ced177311cb850f899de2a2f4e
vsangarya <vishwesharya@gmail.com>
2022-08-29 12:40:01 -0400

    Created setup.py for current functionality of project

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+   name='SE22-group23-HW1',
+   version='1.0',
+   description='Software Engineering Homework Module',
+   author='Atharv Pandit, Muhammad Ali Qureshi, Shivangi Chopra, Vaishnav Nagarajan, Vishwesh Sangarya',
+   author_email='apandit3@ncsu.edu, mquresh@ncsu.edu, schopra4@ncsu.edu, vnagara3@ncsu.edu, lsangar@ncsu.edu', 
+   packages=['SE22-group23-HW1'],
+   install_requires=[], #Will be updated as functionality is added
+)
