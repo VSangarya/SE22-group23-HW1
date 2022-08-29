b621381ac56f29a4740f48f750216fe60179bbf8
Ali Qureshi <qureshi.ali@outlook.com>
2022-08-29 15:51:30 -0400

    bug fix

--- code/finCal.py
+++ code/finCal.py
@@ -12,7 +12,7 @@ def compoundInterest(principal, rate, time):
     if(principal < 0): return "Principal negative, invalid input for function"
 	if(rate < 0): return "Rate negative, invalid input for funtion"
 	if(time < 0): return "Time negative, invalid input for function"
-	return (principal*(1+(rate/100)**time)) - principal
+	return (principal*((1+(rate/100))**time)) - principal
 
 def inflation(principal, rate, time):
     if(principal < 0): return "Principal negative, invalid input for function"
